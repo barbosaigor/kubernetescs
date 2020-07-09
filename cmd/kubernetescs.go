e31d119c5664afb1d5e43601e9faf48314402c59
Igor <igor.barbosa1920@gmail.com>
2020-07-09 11:20:15 -0300

    add: users can change Kubernetes namespace and endpoint by flags

--- cmd/kubernetescs.go
+++ cmd/kubernetescs.go
@@ -7,10 +7,22 @@ import (
 	"github.com/barbosaigor/kubernetescs"
 )
 
+var (
+	// Kubernetes host endpoint
+	endpoint string
+	// Kubernetes namespace
+	namespace string
+)
+
+func init() {
+	cli.RootCmd.Flags().StringVarP(&endpoint, "endpoint", "e", "127.0.0.1:8001", "Enpoint of Kubernetes API")
+	cli.RootCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "Namespace of Kubernetes for CS to operate")
+}
+
 func main() {
 	// Change cli long description
-	cli.RootCmd.Long = "Kubernetescs (Chaos server) shutdowns kubernetes pods via an API."
-	cli.Cs = kubernetescs.ChaosServerKubernetes{Host: "127.0.0.1:8001", Namespace: "lab"}
+	cli.RootCmd.Long = "kubernetescs (Chaos server) shutdowns kubernetes pods via an API."
+	cli.Cs = kubernetescs.ChaosServerKubernetes{Host: endpoint, Namespace: namespace}
 	if err := cli.Execute(); err != nil {
 		log.Printf("Error: %v\n", err)
 	}
